fcee4c67b5afe3cfcbf097bdcf065cc9645c811c
Jaid <jaid.jsx@gmail.com>
2019-07-03 13:48:23 +0200

    Implemented basic functionality
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,44 +1,101 @@
 /** @module key-counter */
 
+import {isString, isArrayLike} from "lodash"
+
 /**
- * Returns the number of seconds passed since Unix epoch (01 January 1970)
  * @example
- * import keyCounter from "key-counter"
- * const result = keyCounter()
- * result === 1549410770
- * @function
- * @returns {number} Seconds since epoch
+ * import KeyCounter from "key-counter"
+ * const counter = KeyCounter()
+ * counter.feed("bananas")
+ * counter.get("bananas") === 1
  */
 export default class {
 
   /**
    * @constructor
-   * @param {string[]|Object<string, number} initialValues
+   * @param {string[]|Object<string, number} [initialValues]
    */
   constructor(initialValues) {
     this.map = {}
+    if (initialValues) {
+      this.feed(initialValues)
+    }
   }
 
   /**
-   * @contructor
    * @param {Object<string, number>}
    */
   feedWithMap(counterMap) {
-
+    for (const [key, incrementValue] of Object.entries(counterMap)) {
+      this.feed(key, incrementValue)
+    }
   }
 
   /**
    * @param {string[]} stringArray
+   * @param {number} [incrementValue=1]
    */
-  feedArray(stringArray) {
-
+  feedWithArray(stringArray, incrementValue) {
+    for (const key of stringArray) {
+      this.feed(key, incrementValue)
+    }
   }
 
   /**
    * @param {string} key
+   * @param {number} [incrementValue=1]
    */
-  feed(key) {
+  feedWithKey(key, incrementValue = 1) {
+    if (!this.map.hasOwnProperty(key)) {
+      this.map[key] = incrementValue
+    } else {
+      this.map[key] += incrementValue
+    }
+  }
 
+  /**
+   * @param {string|string[]|Object<string, number>} value
+   * @param {number} [incrementValue=1]
+   */
+  feed(value, incrementValue = 1) {
+    if (value |> isString) {
+      this.feedWithKey(value, incrementValue)
+    } else if (value |> isArrayLike) {
+      this.feedWithArray(value, incrementValue)
+    } else {
+      this.feedWithMap(value)
+    }
+  }
+
+  /**
+   * @param {string} key
+   * @returns {null|number}
+   */
+  get(key) {
+    return this.map[key] || null
+  }
+
+  /**
+   * @returns {number} Number of different keys
+   */
+  size() {
+    return this.keys().length
+  }
+
+  /**
+   * @returns {string[]} All different keys
+   */
+  keys() {
+    return Object.keys(this.map)
+  }
+
+  /**
+   * @returns {number} Sum of all counts
+   */
+  sum() {
+    return Object.values(this.map).reduce((sum, value) => {
+      return sum + value
+    }, 0)
   }
 
 }
