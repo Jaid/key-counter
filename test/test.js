a96d6a3b42d94f5b75499d02c6e4f61625aa0c1e
Jaid <jaid.jsx@gmail.com>
2019-07-03 13:46:45 +0200

    jest
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -5,9 +5,18 @@ const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.jo
 /**
    * @type { import("../src") }
    */
-const {default: keyCounter} = indexModule
+const {default: KeyCounter} = indexModule
 
 it("should run", () => {
-  const result = keyCounter()
-  expect(result).toBeGreaterThan(1549410770)
+  const counter = new KeyCounter
+  expect(counter).toBeInstanceOf(KeyCounter)
+  expect(counter.size()).toBe(0)
+  counter.feed("peach")
+  counter.feed("peach", 4)
+  expect(counter.size()).toBe(1)
+  expect(counter.get("peach")).toBe(5)
+  counter.feed(["lemon", "banana"], 2)
+  expect(counter.size()).toBe(3)
+  expect(counter.get("lemon")).toBe(2)
+  expect(counter.sum()).toBe(9)
 })
