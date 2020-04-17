79f28533dbc3302d1415978c67ff5e64dd154776
Jaid <jaid.jsx@gmail.com>
2020-04-17 06:26:14 +0200

    Updated tsconfig
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
+import {last, toPairs} from "lodash"
 import path from "path"
-
-import {toPairs, last} from "lodash"
 
 const indexModule = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
 
